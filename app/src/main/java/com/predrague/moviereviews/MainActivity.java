7a4bcc7f7a1de9b7e64a4d9f89175dfd54c0d9e8
Predrague <p.vuckovic.92@gmail.com>
2022-05-04 01:35:03 +0200

    Search UI added

--- app/src/main/java/com/predrague/moviereviews/MainActivity.java
+++ app/src/main/java/com/predrague/moviereviews/MainActivity.java
@@ -3,7 +3,12 @@ package com.predrague.moviereviews;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.predrague.moviereviews.util.network.NetworkConnectionUtil;
@@ -39,5 +44,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         networkConnectionUtil.unregisterNetworkCallbackEvents();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setIconified(false);
+        searchView.setIconifiedByDefault(false);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint(getBaseContext().getString(R.string.search_for_reviews));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                // TODO: Search API
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // TODO: Local search
+                return false;
+            }
+        });
+
+        return true;
     }
 }
